27b3f3efedf48c6353bb5628a0618a513d5f9fe5
iwachacha <s.iwasaki.0904@gmail.com>
2023-08-24 18:01:30 +0900

    vuetifyの導入

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,19 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
+import "vuetify/dist/vuetify.min.css";
+import "@mdi/font/css/materialdesignicons.css"; 
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -14,6 +26,7 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(vuetify)
             .use(ZiggyVue, Ziggy)
             .mount(el);
     },
--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
@@ -25,8 +14,8 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
+Route::get('/home', function () {
+    return Inertia::render('Home');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
